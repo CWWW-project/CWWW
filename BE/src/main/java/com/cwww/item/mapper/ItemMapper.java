78a2680f073920826d76455b356f01641fd10088
dolele092-source <dolele092@gmail.com>
2026-07-15 15:24:27 +0900

    Update ItemMapper.java

--- BE/src/main/java/com/cwww/item/mapper/ItemMapper.java
+++ BE/src/main/java/com/cwww/item/mapper/ItemMapper.java
@@ -8,6 +8,8 @@ import java.util.List;
 @Mapper
 public interface ItemMapper {
 
+
+    //테스트 
     List<Item> findAll();
     List<Item> find(@Param("size") int size, @Param("offset") long offset);
     int countAll();
