0d623329fa77fa49c587c4606962dc48040f3ba0
juwon97 <juwon9714@gamil.com>
2026-07-16 18:35:37 +0900

    🐛 Fix: V12 COUNT() 문법 오류 수정 (COUNT(*) 로 변경)

--- BE/src/main/resources/db/migration/V12__add_users_provider_provider_id.sql
+++ BE/src/main/resources/db/migration/V12__add_users_provider_provider_id.sql
@@ -1,8 +1,8 @@
-SELECT provider, provider_id, COUNT()
+SELECT provider, provider_id, COUNT(*)
 FROM users
 WHERE provider IS NOT NULL
 GROUP BY provider, provider_id
-HAVING COUNT() > 1;
+HAVING COUNT(*) > 1;
 
 -- 제약추가
 ALTER TABLE users
