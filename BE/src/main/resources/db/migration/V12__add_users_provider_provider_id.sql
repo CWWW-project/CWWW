61650f53f93e6634f2ea2eb30c5b3d5ab47dfe23
juwon97 <juwon9714@gamil.com>
2026-07-16 18:12:35 +0900

    ✨ Feat: users테이블 제약추가

---
+++ BE/src/main/resources/db/migration/V12__add_users_provider_provider_id.sql
@@ -0,0 +1,10 @@
+SELECT provider, provider_id, COUNT()
+FROM users
+WHERE provider IS NOT NULL
+GROUP BY provider, provider_id
+HAVING COUNT() > 1;
+
+-- 제약추가
+ALTER TABLE users
+
+    ADD CONSTRAINT uq_users_provider UNIQUE (provider, provider_id);
